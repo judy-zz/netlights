498c2f3996d09378f6936f791f4be450ec9a3816
Clinton Judy <clinton@j-udy.com>
2012-07-12 14:18:53 -0400

    Current version of code.

--- netlights.ino
+++ netlights.ino
@@ -7,12 +7,19 @@ int clockPin = 3;
 int numberOfPixels = 50;
 byte mac[] = { 
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(10,55,55,6); // Use a manual IP Address!
+IPAddress ip(192,168,15,15); // Use a manual IP Address!
 String currentLine;
 String nextColor;
 boolean grabbingColor;
 
-EthernetServer server(80); // (port 80 is default for HTTP):
+int currentRed = 0;
+int currentGreen = 0;
+int currentBlue = 0;
+int nextRed = 0;
+int nextGreen = 0;
+int nextBlue = 0;
+
+EthernetServer server(23); 
 WS2801 strip = WS2801(numberOfPixels, dataPin, clockPin);
 
 void setup() {
@@ -52,11 +59,19 @@ void loop() {
             nextColor += inChar;
           } 
           else {
-            int red   = hexStringToByte(nextColor[0], nextColor[1]);
-            int green = hexStringToByte(nextColor[2], nextColor[3]);
-            int blue  = hexStringToByte(nextColor[4], nextColor[5]);
+            nextRed   = hexStringToInt(nextColor[0], nextColor[1]);
+            nextGreen = hexStringToInt(nextColor[2], nextColor[3]);
+            nextBlue  = hexStringToInt(nextColor[4], nextColor[5]);
             
-            colorWipe(Color(red, green, blue), 50);
+            //colorWipe(Color(nextRed, nextGreen, nextBlue), 100);
+            colorMiddleWipe(Color(nextRed, nextGreen, nextBlue), 100);
+            // colorShow(Color(nextRed, nextGreen, nextBlue));
+            // colorGradient(nextRed, nextGreen, nextBlue, 10);
+
+            currentRed   = hexStringToInt(nextColor[0], nextColor[1]);
+            currentGreen = hexStringToInt(nextColor[2], nextColor[3]);
+            currentBlue  = hexStringToInt(nextColor[4], nextColor[5]);
+            
             resetColorGrabbingProcess();
           }
         }
@@ -74,7 +89,6 @@ void loop() {
     client.stop();
   }
 
-  //  colorWipe(Color(255, 00, 0), 50);
 }
 
 void resetColorGrabbingProcess() {
@@ -83,12 +97,14 @@ void resetColorGrabbingProcess() {
   currentLine = "";
 }
 
-int hexStringToByte(char char1, char char2)
+int hexStringToInt(char char1, char char2)
 {
-  return (hexCharToByte(char1) * 16 + hexCharToByte(char2));
+  // We don't have enough power! Dividing total power by 4...
+  // return (hexCharToInt(char1) * 16 + hexCharToInt(char2));
+  return ((hexCharToInt(char1) * 16 + hexCharToInt(char2)) / 6.1);
 }
 
-int hexCharToByte(char c)
+int hexCharToInt(char c)
 {
   if (c >= 48 && c <= 57) {
     return (c - 48);
@@ -99,12 +115,50 @@ int hexCharToByte(char c)
 }
 
 // fill the dots one after the other with said color
-// good for testing purposes
 void colorWipe(uint32_t c, uint8_t wait) {
   int i;
 
   for (i=0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+// fill the dots one after the other with said color. start from the middle.
+void colorMiddleWipe(uint32_t c, uint8_t wait) {
+  int i;
+  
+  int halfNumOfPixels = strip.numPixels() / 2;
+
+  for (i=0; i < halfNumOfPixels; i++) {
+    strip.setPixelColor(i + halfNumOfPixels, c);
+    strip.setPixelColor(halfNumOfPixels - i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+// fill the the entire strip said color
+void colorShow(uint32_t c) {
+  int i;
+
+  for (i=0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+  }
+  strip.show();
+}
+
+// slowly fade from current color into next color
+void colorGradient(int red, int green, int blue, uint8_t wait) {
+  int i, j;
+
+  for (i=0; i < 80; i += 1) {
+    float percent = i * 0.001;
+    uint32_t color = gradientValue(currentRed, currentGreen, currentBlue, red, green, blue, percent);
+    for (j=0; j < strip.numPixels(); j++) {
+      strip.setPixelColor(j, color);
+    }
     strip.show();
     delay(wait);
   }
@@ -122,6 +176,20 @@ uint32_t Color(byte r, byte g, byte b)
   c <<= 8;
   c |= b;
   return c;
+}
+
+// returns a color between beginning rgb and end rgb colors based upon percent of the range
+uint32_t gradientValue(int br, int bg, int bb, int er, int eg, int eb, float percent) {
+    int red   = br + (percent * (er - br));
+    int green = bg + (percent * (eg - bg));
+    int blue  = bb + (percent * (eb - bb));
+//    if (red   < er && br > er)   { red   = er; };
+//    if (green < eg && bg > eg)   { green = eg; };
+//    if (blue  < eb && bb > eb)   { blue  = eb; };
+//    if (red   > er && br < er)   { red   = er; };
+//    if (green > eg && bg < eg)   { green = eg; };
+//    if (blue  > eb && bb < eb)   { blue  = eb; };
+    return Color(red, green, blue);
 }
 
 //Input a value 0 to 255 to get a color value.
